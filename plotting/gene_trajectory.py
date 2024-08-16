eb99a72a56714e6a1a3226c0215e4081311c053d
Alec Plotkin <alp.plotkin@gmail.com>
2024-08-16 01:39:07 -0400

    Identify some TRACTER clusters in both spleen and gut; regulon trajectories for memory/effector subsets.

--- plotting/gene_trajectory.py
+++ plotting/gene_trajectory.py
@@ -1,11 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import Dict
 from src.sctrat.tools.trajectories import GeneTrajectory
 
 
 def plot_gene_trajectory(
         traj: GeneTrajectory,
         gene: str,
+        color_dict: Dict = None,
 ) -> plt.Axes:
     for subset, df in traj.obs.groupby(traj.subset_var):
         traj_sub = traj[df.index, gene]
@@ -13,7 +15,10 @@ def plot_gene_trajectory(
         std = traj_sub.layers['std'][:, 0]
         se = std / np.sqrt(traj_sub.obs['nobs'])
         time = np.arange(traj_sub.shape[0])
-        plt.plot(time, mean, label=subset)
+        c = None
+        if color_dict is not None:
+            c = color_dict[subset]
+        plt.plot(time, mean, label=subset, c=c)
         plt.fill_between(time, mean - se, mean + se, alpha=0.2)
         plt.xticks(ticks=time, labels=traj_sub.obs[traj.time_var])
     plt.legend()
